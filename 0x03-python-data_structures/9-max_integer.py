1120ed2f0c5c54423aa892120586c5c3b1e8d20b
abdorhl <abdltif.rhl28@gmail.com>
2024-01-06 03:17:51 -0800

    tito

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == []:
-        return(None)
-    my_list.sort()
-    return(my_list[-1])
+        return (min(my_list, key=lambda i: -i)) if my_list else None
